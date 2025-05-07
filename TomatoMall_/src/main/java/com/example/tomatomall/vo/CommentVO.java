faecb3c0b6f3b7b420907c36d74f5e7114a1b685
hushy <1413236998@qq.com>
2025-05-07 14:38:45 +0800

    finish the comment

---
+++ TomatoMall_/src/main/java/com/example/tomatomall/vo/CommentVO.java
@@ -0,0 +1,32 @@
+package com.example.tomatomall.vo;
+
+import com.example.tomatomall.po.Comment;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class CommentVO {
+    private  Integer id;
+    private Integer userId;
+    private Integer productId;
+    private String content;
+    private Float score;
+    private Integer likes;
+    private Date time;
+    public Comment toPO(){
+        Comment comment=new Comment();
+        comment.setId(this.id);
+        comment.setProductId(this.productId);
+        comment.setUserId(this.userId);
+        comment.setLikes(this.likes);
+        comment.setContent(this.content);
+        comment.setScore(this.score);
+        comment.setTime(this.time);
+        return comment;
+    }
+}
