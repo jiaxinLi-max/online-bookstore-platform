ae537ad2a2dcfd8ef9c2839e9abd56215db50743
lijiaxin <2670699786@qq.com>
2025-05-13 16:44:30 +0800

    null

--- vite-project/components.d.ts
+++ vite-project/components.d.ts
@@ -27,6 +27,8 @@ declare module 'vue' {
     ElInputNumber: typeof import('element-plus/es')['ElInputNumber']
     ElMain: typeof import('element-plus/es')['ElMain']
     ElOption: typeof import('element-plus/es')['ElOption']
+    ElRate: typeof import('element-plus/es')['ElRate']
+    ElResult: typeof import('element-plus/es')['ElResult']
     ElRow: typeof import('element-plus/es')['ElRow']
     ElSelect: typeof import('element-plus/es')['ElSelect']
     ElTag: typeof import('element-plus/es')['ElTag']
