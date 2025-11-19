bbbb2a5b03f19fd07e6a6a39d3576ec362bcb02a
hushy <1413236998@qq.com>
2025-11-19 14:36:45 +0800

    add the role of tourist

--- TomatoMall_/src/main/java/com/example/tomatomall/configure/MyWebMvcConfig.java
+++ TomatoMall_/src/main/java/com/example/tomatomall/configure/MyWebMvcConfig.java
@@ -19,6 +19,8 @@ public class MyWebMvcConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/api/images", "/api/images/**")  // 确保排除图片上传相关的路径
                 .excludePathPatterns("/oss/**")  // 排除 OSS 相关路径（根据实际情况修改）
                 .excludePathPatterns("/api/orders/notify")
+                .excludePathPatterns("/api/products")
+                .excludePathPatterns("/api/advertisements")
                 .order(1);
     }
 }
